d587e560726d5b9ecfaee822bef6dc6f0009e76a
Alben <albenbustamante@gmail.com>
2024-08-01 14:21:17 -0500

    add: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TurnsModule } from './turns/turns.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URL), TurnsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URL),
+    TurnsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
